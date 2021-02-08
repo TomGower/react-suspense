c73fdf1de0c0a9b75417350aec78a5710ff38bbb
TomGower <tom.gower@gmail.com>
2021-02-08 17:35:47 -0600

    Solved exercise 01 extra credit 2

--- src/exercise/01ec2.js
+++ src/exercise/01ec2.js
@@ -4,9 +4,11 @@
 import * as React from 'react'
 import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
 
-function createResource(promise) {
+// having throw inside of component functions probably isn't a great idea
+// also, we'll probably be fetching data often, so generic function should be useful
+function createResource(asyncFunc) {
   let status = 'pending'
-  let result = promise.then(
+  let result = asyncFunc.then(
     resolved => {
       status = 'success'
       result = resolved
@@ -18,9 +20,9 @@ function createResource(promise) {
   )
   return {
     read() {
-      if (status === 'pending') throw result
-      if (status === 'error') throw result
-      if (status === 'accept') return result
+      if (status === 'pending') throw result // pending promise
+      if (status === 'error') throw result // rejected promise
+      if (status === 'accept') return result // resolved promise, returning data  
       throw new Error('this should be impossible')
     }
   }
